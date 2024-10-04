70fd0c428f93eaa23982dcf71442e003257cd601
Andes22 <77753631+Natuman-P@users.noreply.github.com>
2024-10-04 23:59:10 +0700

    Create project_7_car_price_prediction.py

---
+++ project_7_car_price_prediction.py
@@ -0,0 +1,154 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+@st.cache_resource
+def read_data():
+    hour_df = pd.read_csv('hour.csv')
+
+    categorical_columns = ['season', 'yr', 'mnth', 'hr', 'holiday', 'weekday', 'workingday', 'weathersit']
+    hour_df[categorical_columns] = hour_df[categorical_columns].astype('category')
+
+    def impute_outliers(data, column):
+        Q1 = data[column].quantile(0.25)
+        Q3 = data[column].quantile(0.75)
+        IQR = Q3 - Q1
+        minimum = Q1 - 1.5 * IQR
+        maximum = Q3 + 1.5 * IQR
+
+        data[column] = data[column].apply(lambda x: maximum if x > maximum else (minimum if x < minimum else x))
+
+    columns_hour = ['hum', 'windspeed', 'casual', 'registered', 'cnt']
+    for column in columns_hour:
+        impute_outliers(hour_df, column)
+
+    return hour_df
+
+
+hour_df = read_data()
+
+st.title("Bike Share Dashboard")
+
+st.sidebar.title("Author:")
+st.sidebar.markdown("**• Nama: Andes Potipera Sitepu**")
+st.sidebar.markdown("**• Email: andessitepu221204@gmail.com**")
+st.sidebar.markdown("**• Dicoding: andes_sitepu**")
+
+if st.sidebar.checkbox("Tampilkan Dataset"):
+    st.subheader("Dataset Original")
+    st.write(hour_df)
+
+if st.sidebar.checkbox("Tampilkan Ringkasan Dataset"):
+    st.subheader("Deskripsi Statistik Dataset")
+    st.write(hour_df.describe())
+
+
+mean_rental_casual_weekend = hour_df[hour_df['holiday'] == 1]['casual'].mean()
+mean_rental_registered_weekend = hour_df[hour_df['holiday'] == 1]['registered'].mean()
+
+
+mean_rental_casual_workingday = hour_df[hour_df['workingday'] == 1]['casual'].mean()  
+mean_rental_registered_workingday = hour_df[hour_df['workingday'] == 1]['registered'].mean()  
+
+text_weekday = f"Rata-rata peminjaman sepeda casual pada hari kerja: {mean_rental_casual_workingday:.2f}\nRata-rata peminjaman sepeda registered pada hari kerja: {mean_rental_registered_workingday:.2f}"
+text_weekend = f"Rata-rata peminjaman sepeda casual pada hari libur: {mean_rental_casual_weekend:.2f}\nRata-rata peminjaman sepeda registered pada hari libur: {mean_rental_registered_weekend:.2f}"
+
+fig1 = go.Figure(data=[
+    go.Bar(x=['Casual', 'Registered'], y=[mean_rental_casual_workingday, mean_rental_registered_workingday], marker_color=['green', 'red'])
+])
+fig1.update_layout(
+    title='Average Bike Rentals on Weekdays',
+    xaxis_title='Rental Type',
+    yaxis_title='Average Rentals',
+    annotations=[dict(x=xi, y=yi, text=f'{yi:.2f}', showarrow=False) for xi, yi in zip(['Casual', 'Registered'], [mean_rental_casual_workingday, mean_rental_registered_workingday])]
+)
+
+fig2 = go.Figure(data=[
+    go.Bar(x=['Casual', 'Registered'], y=[mean_rental_casual_weekend, mean_rental_registered_weekend], marker_color=['blue', 'yellow'])
+])
+fig2.update_layout(
+    title='Average Bike Rentals on Weekends',
+    xaxis_title='Rental Type',
+    yaxis_title='Average Rentals',
+    annotations=[dict(x=xi, y=yi, text=f'{yi:.2f}', showarrow=False) for xi, yi in zip(['Casual', 'Registered'], [mean_rental_casual_weekend, mean_rental_registered_weekend])]
+)
+
+st.plotly_chart(fig1, use_container_width=True)
+st.write(text_weekday)
+st.plotly_chart(fig2, use_container_width=True)
+st.write(text_weekend)
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    st.subheader("Korelasi Situasi Cuaca dan Rental Sepeda")
+
+    weather_count_df = hour_df.groupby("weathersit")["cnt"].sum().reset_index()
+    fig_weather_count = px.bar(weather_count_df, x="weathersit",
+                               y="cnt", title="Situasi Cuaca terhadap Jumlah Rental Sepeda")
+    fig_weather_count.update_traces(marker_color='skyblue')
+
+    st.plotly_chart(fig_weather_count, use_container_width=True, height=400, width=800)
+
+with col2:
+    
+    st.subheader("Korelasi Musim dan Rental Sepeda")
+
+    season_count_df = hour_df.groupby("season")["cnt"].sum().reset_index()
+    fig_season_count = px.bar(season_count_df, x="season",
+                              y="cnt", title="Musim terhadap Jumlah Rental Sepeda")
+    fig_season_count.update_traces(marker_color='salmon')
+    
+    st.plotly_chart(fig_season_count, use_container_width=True, height=400, width=600)
+
+plt.figure(figsize=(14, 24))
+
+st.subheader("Korelasi Jam, Kecepatan Angin, Suhu dan Kelembapan terhadap Jumlah Rental Sepeda")
+
+plt.subplot(6, 1, 1)
+sns.lineplot(data=hour_df, x='hr', y='cnt', color='red')
+plt.title('Pengaruh Hour Terhadap Jumlah Peminjaman Sepeda')
+plt.xlabel('Hour')
+plt.ylabel('Jumlah Peminjaman Sepeda')
+plt.xticks(np.arange(1, 25, 1)) 
+
+fig = plt.gcf()
+
+
+st.pyplot(fig)
+
+
+plt.figure(figsize=(14, 24))
+plt.subplot(6, 1, 2)
+sns.lineplot(data=hour_df, x='windspeed', y='cnt', color='orange')
+plt.title('Pengaruh Windspeed Terhadap Jumlah Peminjaman Sepeda')
+plt.xlabel('Windspeed')
+plt.ylabel('Jumlah Peminjaman Sepeda')
+fig = plt.gcf()
+st.pyplot(fig)
+
+
+plt.figure(figsize=(14, 24))
+plt.subplot(6, 1, 3)
+sns.lineplot(data=hour_df, x='temp', y='cnt', color='green')
+plt.title('Pengaruh Temperature Terhadap Jumlah Peminjaman Sepeda')
+plt.xlabel('Temperature')
+plt.ylabel('Jumlah Peminjaman Sepeda')
+fig = plt.gcf()
+st.pyplot(fig)
+
+
+plt.figure(figsize=(14, 24))
+plt.subplot(6, 1, 4)
+sns.lineplot(data=hour_df, x='hum', y='cnt', color='purple')
+plt.title('Pengaruh Humidity Terhadap Jumlah Peminjaman Sepeda')
+plt.xlabel('Humidity')
+plt.ylabel('Jumlah Peminjaman Sepeda')
+fig = plt.gcf()
+st.pyplot(fig)
